577d6ba6d17fd5052497a83da868cae195793b0b
Jesus <Jesus@Yisus>
2017-03-16 20:13:59 -0400

    Clase planes creada.

---
+++ Claro/src/logical/Planes.java
@@ -0,0 +1,23 @@
+package logical;
+
+public class Planes {
+
+	private String planesInternet;
+
+	public Planes() {
+		// TODO Auto-generated constructor stub
+	}
+	public Planes(String planesInternet) {
+		super();
+		this.planesInternet = planesInternet;
+	}
+
+	public String getPlanesInternet() {
+		return planesInternet;
+	}
+
+	public void setPlanesInternet(String planesInternet) {
+		this.planesInternet = planesInternet;
+	}
+
+}
